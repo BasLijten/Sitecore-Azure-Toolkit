24343a9803c5abe36e7106aeda47c7bec4912803
Bas Lijten <bas.lijten@gmail.com>
2016-12-13 20:15:46 +0100

    initial

---
+++ deploy/deploy.cmd
@@ -0,0 +1,34 @@
+@rem ---------------------------------------------------------------------------------
+@rem Copyright 2008 Microsoft Corporation. All rights reserved.
+@rem This is provided as sample to deploy the package using msdeploy.exe
+@rem For information about IIS Web Deploy technology,
+@rem please visit http://go.microsoft.com/?linkid=9278654
+@rem Note: This batch file assumes the package and setparametsrs.xml are in the same folder with this file
+@rem ---------------------------------------------------------------------------------
+@if %_echo%!==! echo off
+setlocal
+@rem ---------------------------------------------------------------------------------
+@rem Please Make sure you have Web Deploy install in your machine. 
+@rem Alternatively, you can explicit set the MsDeployPath to the location it is on your machine
+@rem set MSDeployPath="C:\Program Files (x86)\IIS\Microsoft Web Deploy V3\"
+@rem ---------------------------------------------------------------------------------
+                      
+@rem ---------------------------------------------------------------------------------
+@rem if user does not set MsDeployPath environment variable, we will try to retrieve it from registry.
+@rem ---------------------------------------------------------------------------------
+if "%MSDeployPath%" == "" (
+for /F "usebackq tokens=1,2,*" %%h  in (`reg query "HKLM\SOFTWARE\Microsoft\IIS Extensions\MSDeploy" /s  ^| findstr -i "InstallPath"`) do (
+if /I "%%h" == "InstallPath" ( 
+if /I "%%i" == "REG_SZ" ( 
+if not "%%j" == "" ( 
+if "%%~dpj" == "%%j" ( 
+set MSDeployPath=%%j
+))))))
+
+if not exist "%MSDeployPath%msdeploy.exe" (
+echo. msdeploy.exe is not found on this machine. Please install Web Deploy before execute the script. 
+echo. Please visit http://go.microsoft.com/?linkid=9278654
+)
+
+
+"%MSDeployPath%msdeploy.exe" -source:package="c:\sitecorewdps\Sitecore813\Sitecore 8.1 rev. 160519_single.scwdp.zip" -dest:auto,IncludeAcls='False' -verb:sync -disableLink:AppPoolExtension -disableLink:ContentExtension -disableLink:CertificateExtension -retryAttempts:2 -setParamFile:"resources\8.1.3\setparameters\single.SetParameters.xml" 
